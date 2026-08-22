edba2d0aeb9c7b2a6ae0abb9af9228da1a990be8
p1t42 <peterfedotovskii@gmail.com>
2026-08-22 19:39:57 +0200

    continued with chapter 6

---
+++ Chapter_6_dictionaires/exercise_batch_2/exercise_people_6_7.py
@@ -0,0 +1,27 @@
+jen = {"Firstname": "jen",
+       "Lastname": "swanson",
+       "food": "chips",
+       "hobby": "working with clay",
+       }
+
+roma = {"Firstname": "roman",
+        "Lastname": "fedtovskii",
+        "food": "govna",
+        "hobby": "football",
+        }
+
+peter = {"Firstname": "peter",
+         "Lastname": "peterson",
+         "food": "fries",
+         "hobby": "coding",
+         }
+
+people = [jen, roma, peter]
+
+for person in people:
+    print(f"\nPerson's name: {person['Firstname']}")
+    print(f"\t{person['Firstname']}'s fullname is {person['Firstname']} {person['Lastname']}")
+    print(f"\t{person['Firstname']}'s favourite food is {person['food']}")
+    print(f"\t{person['Firstname']}'s hobby is {person['hobby']}")
+
+    
